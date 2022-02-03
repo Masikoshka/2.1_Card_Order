6192b2f0d01877bc00dde68c0a9cd248006d45be
Masikoshka <sheve-masha@yandex.ru>
2022-02-03 20:18:06 +0300

    off headless

--- src/test/java/ru/netology/card/CardOrderTest.java
+++ src/test/java/ru/netology/card/CardOrderTest.java
@@ -23,11 +23,12 @@ public class CardOrderTest {
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
